e75af9f3a691aeb43c43340dfe7a3a1ba61ce4e4
Lucy Yang <8395101kktss@gmail.com>
2020-02-22 17:06:48 -0500

    Added logo to nav

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import {Nav, Navbar, Button} from 'react-bootstrap';
+import { Nav, Navbar, Button } from 'react-bootstrap';
+import logo from "../../assets/imgs/mauve-logo.png";
+import '../../assets/css/styles.css';
 
 const Home = () => {
-    return (
-      <>
+  return (
+    <>
       {/* navbar */}
-    <Navbar expand="md" className='pb-4'>
-      <Navbar.Brand href="/">Mauve</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav " />
+      <Navbar expand="md" className='pb-4'>
+        <Navbar.Brand href="/"><img src={logo} alt="logo" className="logo" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav " />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-          <Nav.Item>
-            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Item>
+              <Nav.Link href="#about">About</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-            <Nav.Link href="#portfolio">
+              <Nav.Link href="#portfolio">
                 Work</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
@@ -28,11 +30,11 @@ const Home = () => {
       {/* photo */}
       <div className="home-img">
 
-      <img src="img/womenintech.jpg"/>
+        <img src="img/womenintech.jpg" />
       </div>
       {/* end photo */}
-      </>
-    );
+    </>
+  );
 }
 
 export default Home;
