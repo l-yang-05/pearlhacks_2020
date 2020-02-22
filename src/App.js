66580edb14da2fd234432e88bdcd09faa448e518
Arely <orodeurraca@outlook.com>
2020-02-22 17:16:58 -0500

    updated navigation for site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Home from './Components/Home';
+import Navigation from './Components/Navigation';
 
 function App() {
   return (
+    <>
+    <Navigation />
     <Router>
       <Route exact path="/" component={Home}/>
     </Router>
+    </>
   );
 }
 
