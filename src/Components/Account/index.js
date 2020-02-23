527e808dae74a7751b6760558afadb2642f2fceb
Lucy Yang <8395101kktss@gmail.com>
2020-02-23 01:40:41 -0500

    Editing and moving pfp for account

--- src/Components/Account/index.js
+++ src/Components/Account/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../assets/css/styles.css';
 import UserNavigation from '../UserNavigation';
-
+import pfp from '../../assets/imgs/girl.svg'
 
 const Account = () => {
+    const [interest, setInterest] = useState([]);
+
+    setInterest = ["React.js", "Node.js", "Full-Stack", "MongoDB"];
+
     return (
         <>
             <UserNavigation />
             <div className="account-container">
-
+                <img src={pfp} alt="profile-pic" className="avatar-icon" />
+                <div className="info-container">
+                    <h1>Account Details</h1>
+                    <h2>Name: Lucy Yang</h2>
+                    <h2>Email: ly.yang005@gmail.com</h2>
+                    <h2>Interests: React.js, Node.js, MERN-Stack, Full-Stack</h2>
+                </div>
             </div>
 
         </>
