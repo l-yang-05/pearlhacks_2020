3a6b91c2f2fe525f33b1fd38a9e914cd4ed91078
Arely <orodeurraca@outlook.com>
2020-02-23 01:21:51 -0500

    add files for survey

--- src/Components/Survey/index.js
+++ src/Components/Survey/index.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 import React, {useState} from 'react';
+import Navigation from '../Navigation';
 
 const Button = ({c}) => {
     const [choice, makeChoice] = useState(false)
@@ -20,6 +20,8 @@ const Survey = () => {
             "To find a mentor", "To make friends","To learn new things"
        ] 
     return(
+        <>
+        <Navigation />
         <div>
             <p className="survey-question-one">
                 Tell us a little bit about yourself?
@@ -40,45 +42,8 @@ const Survey = () => {
                 </button>
             </div>
         </div>
-    )
-=======
-import React from 'react';
-import UserNavigation from '../UserNavigation';
-
-const Survey = () => {
-    return (
-        <>
-            <UserNavigation />
-            <div className="survey">
-                <p className="survey-question-one">
-                    Tell us a litte more about yourself...
-        </p>
-
-                <p className="survey-question-two">
-                    Why did you sign up for Mauve?
-        </p>
-
-                <div className="survey-item">
-                    To find a mentor
-            </div>
-
-                <div className="survey-item">
-                    To make friends
-            </div>
-
-                <div className="survey-item">
-                    To learn new things
-            </div>
-
-                <div className="survey-button">
-                    <button >
-                        Submit
-            </button>
-                </div>
-            </div>
         </>
-    );
->>>>>>> 09963b5e228d0b98647efebfff28454a26f6a212
+    )
 }
 
 export default Survey;
