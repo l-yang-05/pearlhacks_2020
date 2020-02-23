bfbd1b9109a673027d3c297b6a7519c215c73131
Arely <orodeurraca@outlook.com>
2020-02-23 01:05:33 -0500

    updated survey page

--- src/Components/Survey/index.js
+++ src/Components/Survey/index.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Survey = () => {
+const Button = ({c}) => {
+    const [choice, makeChoice] = useState(false)
+
+    const handleChoice = () => {
+        makeChoice(!choice)
+    }
     return(
-        <div className="survey">
-        <p className="survey-question-one">
-            Tell us a litte more about yourself...
-        </p>
-
-        <p className="survey-question-two">
-            Why did you sign up for Mauve?
-        </p>
-
-            <div className="survey-item">
-                To find a mentor
-            </div>
-
-            <div className="survey-item">
-                To make friends
-            </div>
-
-            <div className="survey-item">
-                To learn new things
-            </div>
-
-            <div className="survey-button">
-            <button >
-                Submit
-            </button>
-            </div>
+        <div onClick={handleChoice}  className={"survey-item " + (choice ? "success" : "")}>
+            <p className="survey-item-name">{c}</p>
         </div>
     );
+}
+
+
+const Survey = () => {
+    const reasons = [
+            "To find a mentor", "To make friends","To learn new things"
+       ] 
+    return(
+        <div>
+            <p className="survey-question-one">
+                Tell us a little bit about yourself?
+            </p>
+            <p className="survey-question-two">
+                Why do you want to join Mauve?
+            </p>
+            <div className="survey-items">
+            {reasons.map(c => (               
+                <>   
+                <Button c={c}/>
+                </>
+            ))}
+            </div>
+            <div className="survey-button">
+                <button>
+                Submit
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default Survey;
