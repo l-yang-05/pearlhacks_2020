d5c9617575e20e0c9b56e777b00a29a53b913955
Arely <orodeurraca@outlook.com>
2020-02-22 19:39:00 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Home from './Components/Home';
 import Navigation from './Components/Navigation';
+import Survey from './Components/Survey';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Navigation />
     <Router>
       <Route exact path="/" component={Home}/>
+      <Route path="/survey" component={Survey} />
     </Router>
     </>
   );
